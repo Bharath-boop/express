54afd443a76fd12db7ca04a620af31a0d515b5b4
Bharath kumar <bharathboopathi51550@gmail.com>
2024-02-20 15:31:19 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
 import express from "express" //modual js
 import appRoute from './routers/index.js'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT 
-
+app.use(cors())
 app.use(express.json())
 app.use('/', appRoute)
 
